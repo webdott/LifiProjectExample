4a2d3e231c4cb47932684e387106917dafb77f9c
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-04-11 17:38:01 +0100

    start website on Bet_Slip

--- src/components/shared/betInfo/index.tsx
+++ src/components/shared/betInfo/index.tsx
@@ -1,47 +1,47 @@
-import { HeaderButtonText } from "../../../constants/navbar";
-import Button from "../button";
-import { ButtonType } from "../button/type";
-import BetSlip from "./betslip";
-import MyBets from "../myBets";
-import { useState } from "react";
+import { HeaderButtonText } from '../../../constants/navbar';
+import Button from '../button';
+import { ButtonType } from '../button/type';
+import BetSlip from './betslip';
+import MyBets from '../myBets';
+import { useState } from 'react';
 
-import styles from "./betInfo.module.scss";
+import styles from './betInfo.module.scss';
 
 export default function BetInfo(): JSX.Element {
-  const [betContent, setBetContent] = useState<string>(
-    HeaderButtonText.My_Bets
-  );
+	const [betContent, setBetContent] = useState<string>(
+		HeaderButtonText.Bet_Slip
+	);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.betSection}>
-        <div className={styles.topSection}>
-          <div className={styles.betButton}>
-            <Button
-              onClick={() => setBetContent(HeaderButtonText.Bet_Slip)}
-              btnType={
-                betContent === HeaderButtonText.Bet_Slip
-                  ? ButtonType.betInfoButtonActive
-                  : ButtonType.betInfoButton
-              }
-              text={HeaderButtonText.Bet_Slip}
-            />
-            <Button
-              onClick={() => setBetContent(HeaderButtonText.My_Bets)}
-              btnType={
-                betContent === HeaderButtonText.My_Bets
-                  ? ButtonType.betInfoButtonActive
-                  : ButtonType.betInfoButton
-              }
-              text={HeaderButtonText.My_Bets}
-            />
-          </div>
-        </div>
-        <div className={styles.tabContent}>
-          {betContent === HeaderButtonText.My_Bets ? <MyBets /> : <BetSlip />}
-        </div>
-      </div>
-      <div className={styles.playButton}></div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.betSection}>
+				<div className={styles.topSection}>
+					<div className={styles.betButton}>
+						<Button
+							onClick={() => setBetContent(HeaderButtonText.Bet_Slip)}
+							btnType={
+								betContent === HeaderButtonText.Bet_Slip
+									? ButtonType.betInfoButtonActive
+									: ButtonType.betInfoButton
+							}
+							text={HeaderButtonText.Bet_Slip}
+						/>
+						<Button
+							onClick={() => setBetContent(HeaderButtonText.My_Bets)}
+							btnType={
+								betContent === HeaderButtonText.My_Bets
+									? ButtonType.betInfoButtonActive
+									: ButtonType.betInfoButton
+							}
+							text={HeaderButtonText.My_Bets}
+						/>
+					</div>
+				</div>
+				<div className={styles.tabContent}>
+					{betContent === HeaderButtonText.My_Bets ? <MyBets /> : <BetSlip />}
+				</div>
+			</div>
+			<div className={styles.playButton}></div>
+		</div>
+	);
 }
