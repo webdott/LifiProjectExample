5145ea1ba678f80583c468a57aa1685f21f48463
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-04-17 13:58:20 +0100

    layout adjustments

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,86 +1,87 @@
 export interface checkBalanceProps {
-  children: React.ReactNode
+	children: React.ReactNode;
 }
 
 export interface SidebarProps {
-  color: string
+	color: string;
 }
 
 export const sidebarSlide: SidebarProps[] = [
-  { color: '#e6e6e6' },
-  { color: '#33ffc1' },
-  { color: '#505050' },
-]
+	{ color: '#e6e6e6' },
+	{ color: '#33ffc1' },
+	{ color: '#505050' },
+];
 
 export enum SidebarButtonsText {
-  GXP = 'Claim 750 GXP',
-  INVITE1 = 'Mint code 1',
-  INVITE2 = 'Mint code 2',
-  INVITE3 = 'Mint code 3',
-  MINT = 'Mint now',
-  UPGRADE = 'Upgrade to lvl x',
+	GXP = 'Claim 750 GXP',
+	INVITE1 = 'Mint code 1',
+	INVITE2 = 'Mint code 2',
+	INVITE3 = 'Mint code 3',
+	MINT = 'Mint now',
+	UPGRADE = 'Upgrade to lvl x',
 }
 
 export interface NavBarTypes {
-  text: string
-  path: string
+	text: string;
+	path: string;
 }
 
 export interface QuestsTwoTypes {
-  questName: string
-  gxpCount: string
-  rewardsCount: string
+	questName: string;
+	gxpCount: string;
+	rewardsCount: string;
 }
 
 export const navBarLinks: NavBarTypes[] = [
-  {
-    text: 'GXP',
-    path: '/membership',
-  },
-  {
-    text: 'Mint',
-    path: '/mint',
-  },
-  {
-    text: 'Upgrade',
-    path: '/upgrade',
-  },
-  {
-    text: 'Invite',
-    path: '/invite',
-  },
-  {
-    text: 'Help',
-    path: '/help',
-  },
-]
+	{
+		text: 'GXP',
+		path: '/membership',
+	},
+	{
+		text: 'Mint',
+		path: '/mint',
+	},
+	{
+		text: 'Upgrade',
+		path: '/upgrade',
+	},
+	// remove invite page
+	// {
+	//   text: 'Invite',
+	//   path: '/invite',
+	// },
+	{
+		text: 'Help',
+		path: '/help',
+	},
+];
 
 export const gxpValueData: string[] = [
-  'Total GXP earned',
-  'Available GXP',
-  'GXP multiplier',
-]
+	'Total GXP earned',
+	'Available GXP',
+	'GXP multiplier',
+];
 
 export const questsTwoItems: QuestsTwoTypes[] = [
-  {
-    gxpCount: '50',
-    questName: 'Quest one',
-    rewardsCount: '3/3',
-  },
-  {
-    gxpCount: '200',
-    questName: 'Quest two',
-    rewardsCount: '1/1',
-  },
-  {
-    gxpCount: '500',
-    questName: 'Quest three',
-    rewardsCount: '300/300',
-  },
-]
+	{
+		gxpCount: '50',
+		questName: 'Quest one',
+		rewardsCount: '3/3',
+	},
+	{
+		gxpCount: '200',
+		questName: 'Quest two',
+		rewardsCount: '1/1',
+	},
+	{
+		gxpCount: '500',
+		questName: 'Quest three',
+		rewardsCount: '300/300',
+	},
+];
 
 export const invitePageButtons: string[] = [
-  SidebarButtonsText.INVITE1,
-  SidebarButtonsText.INVITE2,
-  SidebarButtonsText.INVITE3,
-]
+	SidebarButtonsText.INVITE1,
+	SidebarButtonsText.INVITE2,
+	SidebarButtonsText.INVITE3,
+];
--- src/layout/GxpPage.tsx
+++ src/layout/GxpPage.tsx
@@ -1,52 +1,54 @@
-import { Fragment, useState } from 'react'
-import { navBarLinks, checkBalanceProps } from '../constants/sidebar'
-import styles from './gxplayout.module.scss'
-import Header from '../components/featured/header'
-import Footer from '../components/featured/footer'
-import { useNavigate } from 'react-router-dom'
-import { useActions } from './../hooks/useActions'
-import { useTypedSelector } from '../hooks/useTypedSelector'
-import BetInfo from '../components/shared/betInfo'
+import { Fragment } from 'react';
+import { navBarLinks, checkBalanceProps } from '../constants/sidebar';
+import Header from '../components/featured/header';
+import Footer from '../components/featured/footer';
+import { useNavigate } from 'react-router-dom';
+import { useActions } from './../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import BetInfo from '../components/shared/betInfo';
+
+import styles from './gxplayout.module.scss';
 
 export default function Sidebar({ children }: checkBalanceProps): JSX.Element {
-  const { getGpxButtonState } = useActions()
-  const { indexNumber } = useTypedSelector((state) => state.gpxButtons)
+	const { getGpxButtonState } = useActions();
+	const { indexNumber } = useTypedSelector((state) => state.gpxButtons);
 
-  const navigate = useNavigate()
+	const navigate = useNavigate();
 
-  const handleNavigation = (path: string, index: number) => {
-    navigate(`${path}`)
+	const handleNavigation = (path: string, index: number) => {
+		navigate(`${path}`);
 
-    getGpxButtonState(index)
-  }
+		getGpxButtonState(index);
+	};
 
-  return (
-    <Fragment>
-      <Header />
-      <div className={styles.section}>
-        <div className={styles.sidebar}>
-          <ul className={styles.sidebarNav}>
-            {navBarLinks.map((el, index) => {
-              return (
-                <button
-                  key={index}
-                  className={
-                    indexNumber === index + 1
-                      ? `${styles.sidebarButton} ${styles.activeTab}`
-                      : `${styles.sidebarButton}`
-                  }
-                  onClick={() => handleNavigation(el.path, index + 1)}
-                >
-                  {el.text}
-                </button>
-              )
-            })}
-          </ul>
-          {children}
-        </div>
-        <BetInfo />
-      </div>
-      <Footer />
-    </Fragment>
-  )
+	return (
+		<Fragment>
+			<Header />
+			<div className={styles.section}>
+				<div className={styles.sidebar}>
+					<ul className={styles.sidebarNav}>
+						{navBarLinks.map((el, index) => {
+							return (
+								<button
+									key={index}
+									className={
+										indexNumber === index + 1
+											? `${styles.sidebarButton} ${styles.activeTab}`
+											: `${styles.sidebarButton}`
+									}
+									onClick={() => handleNavigation(el.path, index + 1)}>
+									{el.text}
+								</button>
+							);
+						})}
+					</ul>
+				</div>
+				<div className={styles.cards}>
+					{children}
+					<Footer />
+				</div>
+				<BetInfo />
+			</div>
+		</Fragment>
+	);
 }
