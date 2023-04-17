42fc5a7c6d5a0197ce5a73b7c4e3449d5f7ae27d
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-04-17 17:30:11 +0100

    redesign upgrade page

--- src/components/shared/upgradePage/index.tsx
+++ src/components/shared/upgradePage/index.tsx
@@ -1,29 +1,24 @@
-import { SidebarButtonsText } from "../../../constants/sidebar";
-import Button from "../button";
-import { ButtonType } from "../button/type";
-import CheckerLayout from "../../../layout/GxpPage";
-import styles from "./upgrade.module.scss";
+import CheckerLayout from '../../../layout/GxpPage';
+import { gxpTokens } from '../../../constants/upgradeGxp';
+import Token from './token';
 
-const MintVideo = require("../../../public/video/MintVideo.mp4");
+import styles from './upgrade.module.scss';
 
 export default function Upgrade() {
-  return (
-    <CheckerLayout>
-      <div className={styles.upgrade}>
-        <div className={styles.upgradeTitle}>Upgrade your Bowtie</div>
-        <div className={styles.upgradeDescription}>Available GXP</div>
-        <div className={styles.upgradeImg}>
-          <video src={MintVideo} width='600' height='300' autoPlay={true} />
-        </div>
-        <div className={styles.pointsSection}>
-          <span>Level x</span>
-          <span>Points to upgrade: x</span>
-          <Button
-            text={SidebarButtonsText.UPGRADE}
-            btnType={ButtonType.claim}
-          />
-        </div>
-      </div>
-    </CheckerLayout>
-  );
+	return (
+		<CheckerLayout>
+			<div className={styles.upgrade}>
+				<div className={styles.upgradeTitle}>Upgrade your Bowtie</div>
+				<div className={styles.upgradeDescription}>
+					<p className={styles.title}>Available GXP</p>
+					<p className={styles.value}>2668</p>
+				</div>
+				<div className={styles.gxpTokens}>
+					{gxpTokens.map((token) => (
+						<Token token={token} key={token.id} />
+					))}
+				</div>
+			</div>
+		</CheckerLayout>
+	);
 }
---
+++ src/components/shared/upgradePage/token.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+
+import { GxpTokenType } from '../../../constants/upgradeGxp';
+import { SidebarButtonsText } from '../../../constants/sidebar';
+import Button from '../button';
+import { ButtonType } from '../button/type';
+
+import styles from './upgrade.module.scss';
+
+interface TokenProps {
+	token: GxpTokenType;
+}
+
+const Token: FC<TokenProps> = ({ token }) => {
+	return (
+		<div className={styles.gxpToken}>
+			<div className={styles.tokenId}>{token.id}</div>
+			<div className={styles.upgradeImg}>
+				<video src={token.urlSrc} autoPlay={true} />
+			</div>
+			<div className={styles.pointsSection}>
+				<span className={styles.level}>
+					Level <span>{token.level}</span>
+				</span>
+				<span className={styles.pointsUpgrade}>
+					Points to upgrade <span>{token.gxpToUpgrade}</span>
+				</span>
+				<Button
+					text={SidebarButtonsText.UPGRADE}
+					btnType={ButtonType.sidebarButton}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Token;
