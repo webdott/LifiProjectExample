31aa46f0553dc771ba812f0db215e5941b85cbd1
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-05-12 11:33:42 +0100

    switch chain on lifi widget experimental

--- src/components/shared/buyWithCryptoPage/index.tsx
+++ src/components/shared/buyWithCryptoPage/index.tsx
@@ -1,7 +1,14 @@
 import { useEffect, useMemo } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { gnosis, polygon } from 'wagmi/chains';
-import { useAccount, useConnect, useDisconnect, useSigner, useSwitchNetwork } from 'wagmi';
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useSigner,
+  useSwitchNetwork,
+  useProvider,
+} from 'wagmi';
 import { HiddenUI, LiFiWidget, WidgetConfig } from '@lifi/widget';
 
 import GetFundsLayout from '../../../layout/GetFundsLayout';
@@ -9,11 +16,11 @@ import ConnectWallet from './connectWallet';
 import { getSelectedChainFromBase } from '../../../functions';
 
 import styles from './buywithcrypto.module.scss';
-import { Connectors } from '../../..';
 
 export default function BuyWithCryptoPage() {
   const { switchNetworkAsync } = useSwitchNetwork();
   const { data: wagmiSigner } = useSigner();
+  const provider = useProvider();
   const { isConnected: walletIsConnected, connector: activeConnector } = useAccount();
   const { connect } = useConnect();
   const { selectedChain } = useParams();
@@ -21,8 +28,10 @@ export default function BuyWithCryptoPage() {
   const location = useLocation();
 
   useEffect(() => {
-
-  }, []);
+    wagmiSigner?.provider?.once('chainChanged', (id: string) => {
+      console.log(id, 'meowmeow');
+    });
+  }, [provider]);
 
   const widgetConfig: WidgetConfig = useMemo(() => {
     const obj: WidgetConfig = {
@@ -33,31 +42,56 @@ export default function BuyWithCryptoPage() {
         // remove OkexChain, Fuse, and Velas from chain list
         deny: [122, 66, 106],
       },
-      // walletManagement: {
-      //   signer: wagmiSigner!,
-      //   connect: async () => {
-      //     connect();
-      //     const signer = await activeConnector?.getSigner();
-      //     if (signer) {
-      //       return signer!;
-      //     } else {
-      //       throw Error('No signer object after login');
-      //     }
-      //   },
-      //   disconnect: async () => {
-      //     disconnect();
-      //   },
-      //   switchChain: async (reqChainId: number) => {
-      //     await switchNetworkAsync?.(reqChainId);
-      //     const signer = await activeConnector?.getSigner();
+      walletManagement: {
+        signer: wagmiSigner!,
+        connect: async () => {
+          connect();
+          if (wagmiSigner) {
+            return wagmiSigner!;
+          } else {
+            throw Error('No signer object after login');
+          }
+        },
+        disconnect: async () => {
+          try {
+            disconnect();
+          } catch (err) {
+            console.log('LiFI Disconnect error');
+          }
+        },
+        switchChain: async (reqChainId: number) => {
+          try {
+            // const switchPromise = new Promise<boolean>((resolve, _) => {
+            //   provider?.once('chainChanged', (id: string) => {
+            //     console.log(id, reqChainId);
+            //     parseInt(id) === reqChainId ? resolve(true) : resolve(false);
+            //   });
+            // });
+            // const chainId = await wagmiSigner?.getChainId();
 
-      //     if (signer) {
-      //       return signer!;
-      //     } else {
-      //       throw Error('No signer object after chain switch');
-      //     }
-      //   },
-      // },
+            const newChain = await switchNetworkAsync?.(reqChainId);
+
+            console.log(newChain, 'hellojello');
+            const signer = await activeConnector?.getSigner();
+            // const switched = await switchPromise;
+            // console.log('SWITCHED:', switched);
+            // if (chainId !== reqChainId) console.log('mememe', chainId, reqChainId);
+
+            // const chainIdAfter = await signer?.getChainId();
+
+            // if (chainIdAfter !== reqChainId) console.log('mememesecond', chainIdAfter, reqChainId);
+
+            if (signer) {
+              return signer!;
+            } else {
+              throw Error('No signer object after chain switch');
+            }
+          } catch (error: any) {
+            console.log('Error occured here');
+            throw Error('No signer object after chain switch');
+          }
+        },
+      },
       containerStyle: {
         width: '100%',
         minWidth: '280px',
